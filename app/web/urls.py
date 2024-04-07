1381211b0b641b03157f7aee7d00babc12451d23
Student <com>
2024-04-07 23:20:35 +0300

    images

--- app/web/urls.py
+++ app/web/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.HomeView.as_view()),
+    path("achievements/", views.AchievementsView.as_view()),
+    path("contacts/", views.ContactsView.as_view()),
+    path("projects/", views.ProjectsView.as_view()),
+    path("services/", views.ServicesView.as_view()),
+    path("documents/", views.DocumentsView.as_view()),
 ]
--- app/web/views.py
+++ app/web/views.py
@@ -3,3 +3,24 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "index.html"
+
+
+class AchievementsView(TemplateView):
+    template_name = "achievements.html"
+
+
+class ContactsView(TemplateView):
+    template_name = "contacts.html"
+    
+
+class ServicesView(TemplateView):
+    template_name = "services.html"
+    
+
+class DocumentsView(TemplateView):
+    template_name = "documents.html"
+    
+
+class ProjectsView(TemplateView):
+    template_name = "projects.html"
+    
